9623bbf5bf0590b66bfb605ddb33b56a1c7a49b1
Alleah Bayas <alleahmariegb@gmail.com>
2026-09-04 00:56:23 +0800

    Added Overview Page and fixed tablet and mobile responsiveness (not yet finalized)

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,21 +1,24 @@
 import "./ProjectCard.css";
+import { Link } from "react-router-dom";
 
-function ProjectCard({ number, title, location, image }) {
+function ProjectCard({ slug, number, title, location, image }) {
     return (
-        <div className="project-card" style={{ backgroundImage: `url(${image})` }}>
-            <div className="project-card-overlay"></div>
-            <span className="project-card-num">{number}</span>
-            <div className="project-card-info">
-                <h3>{title}</h3>
-                <p>{location}</p>
-            </div>
+      <Link to={`/projects/${slug}`} className="project-card-link">
+          <div className="project-card" style={{ backgroundImage: `url(${image})` }}>
+              <div className="project-card-overlay"></div>
+              <span className="project-card-num">{number}</span>
+                 <div className="project-card-info">
+                    <h3>{title}</h3>
+                    <p>{location}</p>
+                 </div>
 
-            <div className="project-card-hover">
-                <span>See</span>
-                <em>Project</em>
-                <span>Overview</span>
-            </div>
-        </div>
+                 <div className="project-card-hover">
+                    <span>See</span>
+                    <em>Project</em>
+                    <span>Overview</span>
+                 </div>
+          </div>
+        </Link>
     );
 }
 
