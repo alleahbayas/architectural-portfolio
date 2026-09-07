7462f2904e9ee431db977bd1421eb0d17d12d278
Alleah Bayas <alleahmariegb@gmail.com>
2026-09-07 15:28:28 +0800

    Fix cloudinary version conflict for Render deploy

--- server/cloudinary.js
+++ server/cloudinary.js
@@ -1,21 +1,18 @@
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 import dotenv from "dotenv";
-import { createRequire } from "module";
 
 dotenv.config();
 
-const require = createRequire(import.meta.url);
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: cloudinary.v2,
   params: {
     folder: "portfolio-projects",
     allowed_formats: ["jpg", "jpeg", "png", "webp"],
